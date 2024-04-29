3ce53fb2ced4ca0610d75f0c8a6ef76013228ce9
Louis Pierce <Louis_Pierce@nevada.unr.edu>
2024-04-29 17:46:31 +0000

    3

--- lou1.c
+++ lou1.c
@@ -1,23 +1,96 @@
-//Author: Louis w Pierce III
-//Program: Mean/Max/Mode/Min/List	
-//Date:4-16-24
-
+//Subject:Image
+//Author:Louis w Pierce lll
+//Date:4-29-24
+	
 #include <stdio.h>
+ int greyScale(int rec1,int rec2);
+ int disPlay(int a,int b);
+ int edit(int chg1,int chg2);
+ 
+ int main(){
+ 	double height, width;
+ 	int rec1,rec2,dis,chg1,chg2;
+ 	char i;
 
-#define FILE_NAME "Design document (LP).txt"
+ 	int images[1];
+ 		printf("Pick image to load, display, and edit.\n");
+									
+ 	for(int i = 0; i < 1; i++){
+ 		printf("(1)Load Image");
+ 		printf("(2)Display Image");
+ 		printf("(3)Edit Image\n");
+ 		scanf("%d", &images[i]);
+ 	}
+		for(int i = 0; i < 1; i++ ) {
+ 		switch (images[i])
+ 	{
+ 		case 1:
+ 				printf("\n");
+ 			printf("Load greyscale image: ");
+ 			scanf("%d", &rec1, &rec2);
+ 			
+ 			height = rec1;
+			width = rec2;
+ 			printf("The height %.2f and the width %.2f of the image.\n ", height,width);
+			
+ 		break;
+ 		case 2:
+ 			printf("\n");
+ 			printf("Select file image to load: ");
+ 			scanf("%d", &dis);
+ 			height = dis;
+ 			width = dis;
+ 			printf("The height %.2f and the width %.2f of the image.\n ", height,width);
 
-	int main() {
-		FILE *fptr;
-		fptr = fopen(FILE_NAME, "w");
+ 		break;
+ 		case 3:
+ 			printf("\n");
+ 			printf("Enter height between (1-600): ");
+ 			scanf("%d", &chg1);
+ 			printf("Enter height between (1-600): ");
+ 			scanf("%d", &chg2);
+ 			height = chg1;
+ 			width = chg2;
+ 			printf("The height %.2f and the width %.2f of the image.\n ", height,width);
+
+		
+ 		break;
+ 		default:
+ 			printf("The picture you have loaded or modified needs to be reloaded!");
+ 		break;
+ 		}
+ 		}
+
+ 	
 	
-		if(fptr ==NULL) {
-		printf("I could'nt open the file!\n");
 	return 0;
-	}
-	
-	fprintf(fptr, "Hello\n");
-	
-	fclose(fptr);
-	
-	return 0;
-	}
+}
+		//case 1 Load image
+			int greyScale(int rec1,int rec2){
+			int height, width;
+				height = rec1;
+				width = rec2;
+			
+		return height,width;
+		}
+			
+		//case 2 Display image
+			int disPlay(int a,int b){
+			int height, width;
+				height = a;
+				width = b;
+				
+		return height,width;
+		}
+			
+			
+ 		//case 3 Edit image
+ 			int edit(int chg1,int chg2){
+ 			int height, width;
+				height = chg1;
+				width = chg2;
+				
+		return height,width;
+		}
+		
+			
